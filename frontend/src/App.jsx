37818394656311c47ebc7e6912ce495521c3126f
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2026-04-24 00:12:55 +0200

    First commit (Start)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Header from "./components/header";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Header/>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 100)}>Öka</button>
+    </div>
+  );
+};
+
+export default App;

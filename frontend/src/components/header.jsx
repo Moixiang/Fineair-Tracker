16db4a45d868ca4a75fa17d316922947bc19c4c1
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2026-04-26 21:25:23 +0200

    Favicon & Header (navigation)

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,9 +1,27 @@
 const Header = () => {
   return (
     <header>
-      <nav>Hej</nav>
+      <nav className="navigationContainer">
+        <div className="sideLogo">
+          <ul>
+            <li>
+              <a href="#top">Fine</a>
+            </li>
+          </ul>
+          <span>SKY IS THE LIMIT</span>
+        </div>
+
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search tail number, aircraft or airport..."
+          />
+        </div>
+
+        <div className="hamburgerButton"></div>
+      </nav>
     </header>
-  )
+  );
 };
 
 export default Header;

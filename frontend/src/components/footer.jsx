0289dbc556cb96dd1b98c1df8ec4e7bead747c3d
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2026-06-07 01:11:24 +0200

    Footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../footer.css";
 
 const Footer = () => {
   const [email, setEmail] = useState("");
@@ -12,7 +13,7 @@ const Footer = () => {
 
   return (
     <div>
-      <footer>
+      <section>
         <div className="footerContainer">
           <input
             type="email"
@@ -21,6 +22,38 @@ const Footer = () => {
           />
           <button onClick={getEmail}></button>
         </div>
+      </section>
+
+      <footer>
+        <nav className="footerNavigationContainer">
+          <div className="siteLogo">
+            <ul>
+              <li>
+                <a href="#top">
+                  <span className="fineAir">FINE AIR</span>
+                  <span className="slogan">SKY IS THE LIMIT</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footerLinks">
+            <ul>
+              <li>
+                <a href="#">Live Map</a>
+                <a href="#">Aircraft</a>
+                <a href="#">Airports</a>
+                <a href="#">Alerts</a>
+                <a href="#">About</a>
+                <a href="#">Privacy</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="copyright">
+            <span>© 2026 Fine Air</span>
+          </div>
+        </nav>
       </footer>
     </div>
   );

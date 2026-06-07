407118603f2c5f23873352a3948a93ac2eeb02f4
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2026-06-07 14:53:10 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,35 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { useEffect, useState } from "react";
+import Home from "./pages/home";
 
 const App = () => {
   const [data, setData] = useState();
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch("https://jsonplaceholder.typicode.com/users", {
-        method: "GET"
-      })
-      const result = await data.json()
-      console.log(result)
-      setData(result)
-    }
-    fetchData()
-  }, [])
+        method: "GET",
+      });
+      const result = await data.json();
+      console.log(result);
+      setData(result);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <Header />
-      {
-      data ? (
-      <p>Address: {data[0]?.address?.zipcode}</p>
-      ) :
-      (<p>Laddar...</p>)
-      }
-      <Footer />
-    </div> 
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        {data ? <p>Address: {data[0]?.address?.zipcode}</p> : <p>Laddar...</p>}
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 

e6a310e0ec59eb5d8481e494184381e85329a133
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2026-09-03 16:21:20 +0200

    Media Q for home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Home from "./pages/home";
+import "./homeMediaQ.css"
+
 
 const App = () => {
   return (
